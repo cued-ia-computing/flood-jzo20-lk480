25f847156f5e63a3a01be2969f5e2b8eb831954f
Jia Zheng Ong <jzo20@cam.ac.uk>
2021-02-26 19:18:56 +0000

    Task 2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -22,3 +22,28 @@ def stations_level_over_threshold(stations, tol):
         if relative_water_level is not None and relative_water_level > tol:
             stations_over_threshold.append((station, station.relative_water_level()))
     return sorted_by_key(stations_over_threshold, 1, reverse=True)
+
+
+def stations_highest_rel_level(stations, N):
+    """Returns a list of N tuples containing stations(objects) and their relative water level
+
+    Args:
+        stations (list[tuples]): Monitoring Station Objects
+        N ([float]): Number of water station
+
+    Returns:
+        [list(tuples)]: list of N tuples containing station objects and relative water level, sorted in descending order
+    """
+    stations_at_risk = []
+    for station in stations:
+        relative_water_level = station.relative_water_level()
+        if relative_water_level is not None:
+            stations_at_risk.append((station.name, relative_water_level))
+
+    sorted_list = sorted_by_key(stations_at_risk, 1, reverse=True)
+
+    N_stations_at_risk = []
+    for i in range(N):
+        N_stations_at_risk.append((sorted_list[i]))
+
+    return N_stations_at_risk
