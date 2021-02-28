5c7ec99d9bbe4a4f62764d0938d6936fe27de804
Lohith Konathala <lk480@cam.ac.uk>
2021-02-28 18:13:56 +0000

    stations_highest_rel_level to use function from 2B

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -6,12 +6,12 @@ the locations of the DEFRA Monitoring Stations
 from floodsystem.utils import sorted_by_key  # noqa
 
 
-def stations_level_over_threshold(stations, tol):
+def stations_level_over_threshold(stations, tol=None):
     """Returns a list of tuples containing station objects and relative water level
 
     Args:
         stations (list[tuples]): Monitoring Station Objects
-        tol ([float]): Threshold Value
+        tol ([float, None]): Threshold Value which defaults to None
 
     Returns:
         [list(tuples)]: list of tuples containing station objects and relative water level, sorted in descending order
@@ -19,8 +19,8 @@ def stations_level_over_threshold(stations, tol):
     stations_over_threshold = []
     for station in stations:
         relative_water_level = station.relative_water_level()
-        if relative_water_level is not None and relative_water_level > tol:
-            stations_over_threshold.append((station, station.relative_water_level()))
+        if relative_water_level is not None and (tol is None or relative_water_level > tol):
+            stations_over_threshold.append((station, relative_water_level))
     return sorted_by_key(stations_over_threshold, 1, reverse=True)
 
 
@@ -34,16 +34,11 @@ def stations_highest_rel_level(stations, N):
     Returns:
         [list(tuples)]: list of N tuples containing station objects and relative water level, sorted in descending order
     """
-    stations_at_risk = []
-    for station in stations:
-        relative_water_level = station.relative_water_level()
-        if relative_water_level is not None:
-            stations_at_risk.append((station.name, relative_water_level))
-
-    sorted_list = sorted_by_key(stations_at_risk, 1, reverse=True)
+    sorted_list = stations_level_over_threshold(stations)
 
     N_stations_at_risk = []
     for i in range(N):
-        N_stations_at_risk.append((sorted_list[i]))
+        station, relative_water_level = sorted_list[i]
+        N_stations_at_risk.append((station.name, relative_water_level))
 
     return N_stations_at_risk
