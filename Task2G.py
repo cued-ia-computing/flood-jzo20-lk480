d8772aba27fb5c0f72220f7e572d9e30c7484260
Jia Zheng Ong <jzo20@cam.ac.uk>
2021-03-03 13:29:32 +0000

    Completed 2G

--- Task2G.py
+++ Task2G.py
@@ -1,5 +1,5 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.flood import stations_highest_rel_level
+from floodsystem.warning import floodwarning
 
 
 def run():
@@ -9,18 +9,10 @@ def run():
     # Update latest level data for all stations
     update_water_levels(stations)
 
-    # Obtain list of stations with highest relative water level
-    result = stations_highest_rel_level(stations, 10)
-
-    for station, relative_level in result:
-        print(f"{station}: {relative_level}")
-
-    n = 0
-    if n < 10:
-        print(stations[n])
-        n += 1
+    flood_warning = floodwarning(stations, 100)
+    print(flood_warning.get_risky_areas())
 
 
 if __name__ == "__main__":
-    print("*** Task 2C: CUED Part IA Flood Warning System ***")
+    print("*** Task 2G: CUED Part IA Flood Warning System ***")
     run()
